d2b41f17a43d41f44458766ea2dbdec1e7551b3e
Lucian Cumpata <cumpata.lu@outlook.com>
2018-12-24 21:19:29 +0200

    Added french translation

--- js/i18n.js
+++ js/i18n.js
@@ -46,8 +46,57 @@ let locales = {
         difficulty: "Dificultate",
         game: "Joc",
         puzzleGeneration: "Generare puzzle"
-    }
+    },
     
+    ru: {
+        helpAbout: "О нас",
+        helpMadeBy: "Разработано",
+        helpPlay: "играть",
+        helpDescribePlay: "Acesta este butonul de incepere a jocului",
+        helpSolvePiece: "Rezolva piesa",
+        helpDescribeSolvePiece: "Acesta este butonul de rezolvare a unei piese.",
+        helpDescribeSolvePiece1: "Rezolva in mod aleatoriu sau rezolva piesa curenta.",
+        language: "RU",
+        progress: "прогресс",
+        play: "играть",
+        solvePiece: "решить фрагмент",
+        currentlySelected: "выбранного",
+        random: "случайный",
+        reset: "сбросить",
+        load: "загрузить",
+        chooseImageToUpload: "Выберите изображение для загрузки",
+        enterImageUrl: "Введите URL-адрес изображения",
+        loadSavedGame: "Загрузка сохраненные игры",
+        save: "сохранить",
+        difficulty: "сложности",
+        game: "игра",
+        puzzleGeneration: "Генерировать головоломку"
+    },
+
+    fr: {
+        helpAbout: "À propos", 
+        helpMadeBy: "Faite par",
+        helpPlay: "Jouer",
+        helpDescribePlay: "Acesta este butonul de incepere a jocului",
+        helpSolvePiece: "Rezolva piesa",
+        helpDescribeSolvePiece: "Acesta este butonul de rezolvare a unei piese.",
+        helpDescribeSolvePiece1: "Rezolva in mod aleatoriu sau rezolva piesa curenta.",
+        language: "FR",
+        progress: "progrès",
+        play: "Jouer",
+        solvePiece: "Résoudre la pièce",
+        currentlySelected: "Actuellement sélectionné",
+        random: "Aléatoire",
+        reset: "Réinitialiser",
+        load: "Charge",
+        chooseImageToUpload: "Choisissez l’image à télécharger",
+        enterImageUrl: "Entrez l’URL de l’image",
+        loadSavedGame: "Jeu de charge sauvé",
+        save: "Sauver",
+        difficulty: "Difficulté",
+        game: "Jeu",
+        puzzleGeneration: "Générer des puzzle"
+    }
 }
 
 function updateLocale(locale) {
