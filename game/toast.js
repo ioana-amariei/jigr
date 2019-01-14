f63a55e85c1b013d78a38d8fc209bcd04ccd5d48
ioanabirsan <ioana.c.birsan@gmail.com>
2019-01-14 08:33:15 +0200

    Change toast duration

--- game/toast.js
+++ game/toast.js
@@ -1,13 +1,16 @@
+// Options to override toast style and settings
 let options = {
 	style: {
 		main: {
-			// Options to override toast style
 			width: '25%',
 			background: '#ebf5fb',
 			color: '#2980b9',
 			padding: '25px 15px',
-			'box-shadow': '4px 4px #2980b9',
+			'box-shadow': '4px 4px 4px #2980b9',
 			'border-radius': '50px'
 		}
+	},
+	settings: {
+		duration: '2000'
 	}
 };
